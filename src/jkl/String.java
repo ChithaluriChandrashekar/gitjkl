40b8673fb16a14a41bcaf01c176796017850c8e3
chithaluri chandrashekar <chandrasheker976@gmail.com>
2023-05-31 19:33:29 +0530

    first commit

---
+++ src/jkl/String.java
@@ -0,0 +1,14 @@
+package jkl;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		char r;
+// java.lang.StringTODO Auto-generated method stub
+   char[] ch= {c,h,a,n,d,r,a,s,h,e,k,a,r};
+    
+    String 
+		 
+	}
+
+}
